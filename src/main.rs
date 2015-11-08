d39ba3d9259785aedd397db074bec276b7dde684
@zip <zip@keybase.io>
2015-11-07 19:04:14 -0800

    Tidy up

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-//#![allow(dead_code)]
-
 mod wave;
 mod sample;
 mod dtmf;
@@ -7,8 +5,7 @@ mod dtmf;
 use dtmf::DTMF;
 
 fn main() {
-    // DTMF 1
-    let sample_stream = DTMF::dial("1296");
+    let sample_stream = DTMF::dial("0123456789");
     sample_stream.write("tone.wav");
     println!("{}", sample_stream);
 }
